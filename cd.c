4947a191c0bcc28eb7447f5104b4551acba6108c
Ashevita <9362@holbertonstudents.com>
2024-08-23 09:49:49 +0200

    com anglais

--- cd.c
+++ cd.c
@@ -1,10 +1,10 @@
 #include "simple_shell.h"
 
 /**
- * execute_cd - Gère la commande "cd".
- * @line: La ligne de commande entrée par l'utilisateur.
- * @home_directory: Le chemin du répertoire HOME.
- * @previous_directory: Le chemin du répertoire utilisé par la commande 'cd'.
+ * execute_cd - Handles the "cd" command.
+ * @line: The command line entered by the user.
+ * @home_directory: The path to the HOME directory.
+ * @previous_directory: The path to the directory used by the 'cd' command.
  */
 void execute_cd(const char *line, const char *home_directory,
 		char *previous_directory)
@@ -13,7 +13,7 @@ void execute_cd(const char *line, const char *home_directory,
 	char current_directory[1024];
 
 	if (path == NULL || strcmp(path, "~") == 0)
-		path = (char *)home_directory; /* Aller au répertoire HOME par défaut */
+		path = (char *)home_directory; /* Go to the HOME directory by default */
 	else if (strcmp(path, "-") == 0)
 	{
 		if (strlen(previous_directory) == 0)
@@ -21,15 +21,15 @@ void execute_cd(const char *line, const char *home_directory,
 			printf("simple_shell: cd: OLDPWD not set\n");
 			return;
 		}
-		path = previous_directory; /* Aller au répertoire précédent */
+		path = previous_directory; /* Go to the previous directory */
 	}
 
-	/* Mettre à jour répertoire précédent avant de changer de répertoire */
+	/* Update the previous directory before changing the directory */
 	if (getcwd(current_directory, sizeof(current_directory)) != NULL)
-	strncpy(previous_directory, current_directory,
-	sizeof(previous_directory) - 1);
+		strncpy(previous_directory, current_directory,
+			sizeof(previous_directory) - 1);
 
-	/* Changer de répertoire */
+	/* Change the directory */
 	if (chdir(path) != 0)
 		perror("cd");
 }
